a0fe62ab71c5b19e45cb454679d6d4e640932d4d
Vitor Augusto da Silva Haselei <augusto@publi.com.br>
2019-06-03 13:35:51 -0300

    first

---
+++ app/__init__.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
+
+import numpy as np 
+
+app = Flask(__name__)
+app.config.from_object('config')
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///storage.db'
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+
+manager = Manager(app)
+manager.add_command('db', MigrateCommand)
+
+
+
+
+from app.controllers import default
+
---
+++ app/controllers/default.py
@@ -0,0 +1,13 @@
+from flask import render_template
+from app import app
+
+
+@app.route("/index/")
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+@app.route("/login")
+def login():
+    return render_template('base.html')
+            
